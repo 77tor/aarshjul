7625f8a0acafeb165cf967027dd5e9f82bc3604f
77tor <77tor@ikrs.no>
2026-08-10 10:59:25 +0200

    Add files via upload

---
+++ fellesaktiviteter.js
@@ -0,0 +1,481 @@
+/**
+ * Fellesaktiviteter – Oversikt over alle skoleår
+ */
+
+const FELLESAKTIVITETER_REGISTER = {
+  
+  // =========================================================================
+  // SKOLEÅRET 2026 / 2027
+  // =========================================================================
+  "2026-2027": [
+    // --- AUGUST 2026 ---
+    {
+      id: "fa-2627-08-1",
+      tittel: "Fadder-ordning",
+      maaned: "August",
+      type: "Uke",
+      uke: "Uke 33-34",
+      fraDato: "2026-08-10",
+      tilDato: "2026-08-23",
+      deltakere: "Alle trinn",
+      ansvar: "Alle trinn"
+    },
+    {
+      id: "fa-2627-08-2",
+      tittel: "Kildesortering",
+      maaned: "August",
+      type: "Uke",
+      uke: "Uke 33-35",
+      fraDato: "2026-08-10",
+      tilDato: "2026-08-30",
+      deltakere: "Alle trinn",
+      ansvar: "Alle trinn"
+    },
+
+    // --- SEPTEMBER 2026 ---
+    {
+      id: "fa-2627-09-1",
+      tittel: "Beintøft",
+      maaned: "September",
+      type: "Uke",
+      uke: "Uke 36",
+      fraDato: "2026-08-31",
+      tilDato: "2026-09-06",
+      deltakere: "Alle trinn",
+      ansvar: "Alle trinn"
+    },
+    {
+      id: "fa-2627-09-2",
+      tittel: "Brannvernuka",
+      maaned: "September",
+      type: "Uke",
+      uke: "Uke 38",
+      fraDato: "2026-09-14",
+      tilDato: "2026-09-20",
+      deltakere: "6. trinn",
+      ansvar: "6. trinn"
+    },
+    {
+      id: "fa-2627-09-3",
+      tittel: "BlimE",
+      maaned: "September",
+      type: "Dato",
+      dato: "2026-09-18",
+      deltakere: "Alle trinn",
+      ansvar: "3. trinn og 4. trinn"
+    },
+    {
+      id: "fa-2627-09-4",
+      tittel: "Aktivitetsdag",
+      maaned: "September",
+      type: "Dato",
+      dato: "2026-09-25",
+      deltakere: "Alle trinn",
+      ansvar: "7. trinn"
+    },
+
+    // --- OKTOBER 2026 ---
+    {
+      id: "fa-2627-10-1",
+      tittel: "FN-samling",
+      maaned: "Oktober",
+      type: "Dato",
+      dato: "2026-10-23",
+      deltakere: "Brattbakken",
+      ansvar: "Elevrådet"
+    },
+    {
+      id: "fa-2627-10-2",
+      tittel: "Leseuke – Brattbakken",
+      maaned: "Oktober",
+      type: "Uke",
+      uke: "Uke 42",
+      fraDato: "2026-10-12",
+      tilDato: "2026-10-18",
+      deltakere: "Brattbakken",
+      ansvar: "5. trinn"
+    },
+    {
+      id: "fa-2627-10-3",
+      tittel: "Lekeuke – Heståsen",
+      maaned: "Oktober",
+      type: "Uke",
+      uke: "Uke 43",
+      fraDato: "2026-10-19",
+      tilDato: "2026-10-25",
+      deltakere: "Heståsen",
+      ansvar: "3. trinn"
+    },
+    {
+      id: "fa-2627-10-4",
+      tittel: "Sangsamling - Heståsen",
+      maaned: "Oktober",
+      type: "Dato",
+      dato: "2026-10-28",
+      deltakere: "Heståsen",
+      ansvar: "2. trinn"
+    },
+    {
+      id: "fa-2627-10-5",
+      tittel: "Kirka - solidaritet",
+      maaned: "Oktober",
+      type: "Uavklart",
+      dato: null,
+      deltakere: "6. trinn",
+      ansvar: "Kirka"
+    },
+
+    // --- NOVEMBER 2026 ---
+    {
+      id: "fa-2627-11-1",
+      tittel: "Fadder-ordning",
+      maaned: "November",
+      type: "Uke",
+      uke: "Uke 45-46",
+      fraDato: "2026-11-02",
+      tilDato: "2026-11-15",
+      deltakere: "Alle trinn",
+      ansvar: "Alle trinn"
+    },
+    {
+      id: "fa-2627-11-2",
+      tittel: "Kirka - bibelen",
+      maaned: "November",
+      type: "Uavklart",
+      dato: null,
+      deltakere: "5. trinn",
+      ansvar: "Kirka"
+    },
+    {
+      id: "fa-2627-11-3",
+      tittel: "Sangsamling – begge skoler",
+      maaned: "November",
+      type: "Uke",
+      uke: "Uke 47-48",
+      fraDato: "2026-11-16",
+      tilDato: "2026-11-29",
+      deltakere: "Heståsen og Brattbakken",
+      ansvar: "1. trinn og 4. trinn"
+    },
+
+    // --- DESEMBER 2026 ---
+    {
+      id: "fa-2627-12-1",
+      tittel: "Lucia",
+      maaned: "Desember",
+      type: "Dato",
+      dato: "2026-12-11",
+      deltakere: "Alle trinn",
+      ansvar: "2. trinn"
+    },
+    {
+      id: "fa-2627-12-2",
+      tittel: "Kirka - Julevandring",
+      maaned: "Desember",
+      type: "Uavklart",
+      dato: null,
+      deltakere: "1. trinn",
+      ansvar: "Kirka"
+    },
+    {
+      id: "fa-2627-12-3",
+      tittel: "Juleverksted",
+      maaned: "Desember",
+      type: "Dato",
+      dato: "2026-12-16",
+      deltakere: "Alle trinn",
+      ansvar: "Komité + 1. trinn og 6. tr."
+    },
+    {
+      id: "fa-2627-12-4",
+      tittel: "Kirka - Julegudstjeneste",
+      maaned: "Desember",
+      type: "Uavklart",
+      dato: null,
+      deltakere: "Alle trinn",
+      ansvar: "3. trinn"
+    },
+
+    // --- JANUAR 2027 ---
+    {
+      id: "fa-2627-01-1",
+      tittel: "Vinteraktivitetsdag",
+      maaned: "Januar",
+      type: "Dato",
+      dato: "2027-01-22",
+      deltakere: "Alle trinn",
+      ansvar: "5. trinn"
+    },
+    {
+      id: "fa-2627-01-2",
+      tittel: "Fadder-ordning",
+      maaned: "Januar",
+      type: "Uke",
+      uke: "Uke 2-3",
+      fraDato: "2027-01-11",
+      tilDato: "2027-01-24",
+      deltakere: "Alle trinn",
+      ansvar: "Alle trinn"
+    },
+
+    // --- FEBRUAR 2027 ---
+    {
+      id: "fa-2627-02-1",
+      tittel: "Karneval",
+      maaned: "Februar",
+      type: "Dato",
+      dato: "2027-02-04",
+      deltakere: "Heståsen og 4. trinn",
+      ansvar: "3. trinn og 7. trinn"
+    },
+    {
+      id: "fa-2627-02-2",
+      tittel: "Engelskdag",
+      maaned: "Februar",
+      type: "Dato",
+      dato: "2027-02-12",
+      deltakere: "Brattbakken",
+      ansvar: "7. trinn"
+    },
+    {
+      id: "fa-2627-02-3",
+      tittel: "Kirka - kirkerom og symboler",
+      maaned: "Februar",
+      type: "Uavklart",
+      dato: null,
+      deltakere: "4. trinn",
+      ansvar: "4. trinn"
+    },
+
+    // --- MARS 2027 ---
+    {
+      id: "fa-2627-03-1",
+      tittel: "Batterijakten",
+      maaned: "Mars",
+      type: "Uke",
+      uke: "Uke 10-12",
+      fraDato: "2027-03-08",
+      tilDato: "2027-03-28",
+      deltakere: "4. trinn",
+      ansvar: "4. trinn"
+    },
+    {
+      id: "fa-2627-03-2",
+      tittel: "Fadder-ordning",
+      maaned: "Mars",
+      type: "Uke",
+      uke: "Uke 12-13",
+      fraDato: "2027-03-22",
+      tilDato: "2027-04-04",
+      deltakere: "Alle trinn",
+      ansvar: "Alle trinn"
+    },
+    {
+      id: "fa-2627-03-3",
+      tittel: "Påskevandring",
+      maaned: "Mars",
+      type: "Uavklart",
+      dato: null,
+      deltakere: "2. trinn",
+      ansvar: "2. trinn"
+    },
+
+    // --- APRIL 2027 ---
+    {
+      id: "fa-2627-04-1",
+      tittel: "Leseuke - Heståsen",
+      maaned: "April",
+      type: "Uke",
+      uke: "Uke 16",
+      fraDato: "2027-04-19",
+      tilDato: "2027-04-25",
+      deltakere: "Heståsen",
+      ansvar: "3. trinn"
+    },
+    {
+      id: "fa-2627-04-2",
+      tittel: "Sangsamling",
+      maaned: "April",
+      type: "Uke",
+      uke: "Uke 17-18",
+      fraDato: "2027-04-26",
+      tilDato: "2027-05-09",
+      deltakere: "Alle trinn",
+      ansvar: "1. trinn og 4. trinn"
+    },
+
+    // --- MAI 2027 ---
+    {
+      id: "fa-2627-05-1",
+      tittel: "Generalprøve-stjerner i siktet",
+      maaned: "Mai",
+      type: "Dato",
+      dato: "2027-05-04",
+      deltakere: "Deltakere",
+      ansvar: "6. trinn"
+    },
+    {
+      id: "fa-2627-05-2",
+      tittel: "Stjerner i siktet",
+      maaned: "Mai",
+      type: "Dato",
+      dato: "2027-05-05",
+      deltakere: "Brattbakken",
+      ansvar: "6. trinn"
+    },
+    {
+      id: "fa-2627-05-3",
+      tittel: "Kirka - Pinse/dåp",
+      maaned: "Mai",
+      type: "Uavklart",
+      dato: null,
+      deltakere: "3. trinn",
+      ansvar: "3. trinn"
+    },
+    {
+      id: "fa-2627-05-4",
+      tittel: "Sykkelprøve",
+      maaned: "Mai",
+      type: "Uke",
+      uke: "Uke 18",
+      fraDato: "2027-05-03",
+      tilDato: "2027-05-09",
+      deltakere: "4. trinn",
+      ansvar: "4. trinn"
+    },
+    {
+      id: "fa-2627-05-5",
+      tittel: "Matematikkdag",
+      maaned: "Mai",
+      type: "Dato",
+      dato: "2027-05-21",
+      deltakere: "Alle trinn",
+      ansvar: "2. trinn og 5. trinn"
+    },
+
+    // --- JUNI 2027 ---
+    {
+      id: "fa-2627-06-1",
+      tittel: "Miljøuka",
+      maaned: "Juni",
+      type: "Uke",
+      uke: "Uke 23",
+      fraDato: "2027-06-07",
+      tilDato: "2027-06-13",
+      deltakere: "Alle trinn",
+      ansvar: "Alle trinn"
+    },
+    {
+      id: "fa-2627-06-2",
+      tittel: "Fadder-ordning",
+      maaned: "Juni",
+      type: "Uke",
+      uke: "Uke 22-23",
+      fraDato: "2027-05-31",
+      tilDato: "2027-06-13",
+      deltakere: "Alle trinn",
+      ansvar: "Alle trinn"
+    },
+    {
+      id: "fa-2627-06-3",
+      tittel: "Kirka - \"Hvem er jeg?\"",
+      maaned: "Juni",
+      type: "Uavklart",
+      dato: null,
+      deltakere: "7. trinn",
+      ansvar: "7. trinn"
+    },
+    {
+      id: "fa-2627-06-4",
+      tittel: "Åpen skole",
+      maaned: "Juni",
+      type: "Dato",
+      dato: "2027-06-09",
+      deltakere: "1.-6. trinn",
+      ansvar: "FAU"
+    },
+    {
+      id: "fa-2627-06-5",
+      tittel: "Uteskoledag",
+      maaned: "Juni",
+      type: "Dato",
+      dato: "2027-06-11",
+      deltakere: "1.-3. trinn",
+      ansvar: "1. trinn"
+    },
+    {
+      id: "fa-2627-06-6",
+      tittel: "Fotballdag",
+      maaned: "Juni",
+      type: "Dato",
+      dato: "2027-06-11",
+      deltakere: "4.-7. trinn",
+      ansvar: "5. trinn"
+    },
+    {
+      id: "fa-2627-06-7",
+      tittel: "Kulturtimen",
+      maaned: "Juni",
+      type: "Dato",
+      dato: "2027-06-18",
+      deltakere: "1.-6. trinn",
+      ansvar: "2. trinn"
+    }
+  ],
+
+  // =========================================================================
+  // EKSEMPEL: SKOLEÅRET 2027 / 2028 (Kan fylles ut senere)
+  // =========================================================================
+  "2027-2028": [
+    /* Nye aktiviteter legges til her */
+  ]
+};
+
+// =========================================================================
+// HJELPEFUNKSJONER
+// =========================================================================
+
+/**
+ * Henter rådata for enten et spesifikt skoleår eller alle år flettet sammen.
+ * @param {string} [skoleaar] F.eks. "2026-2027". Om utelatt returneres alle.
+ */
+function getFellesaktiviteter(skoleaar) {
+  if (skoleaar && FELLESAKTIVITETER_REGISTER[skoleaar]) {
+    return FELLESAKTIVITETER_REGISTER[skoleaar];
+  }
+  
+  // Hvis ikke valgt, returneres alle år slått sammen
+  return Object.values(FELLESAKTIVITETER_REGISTER).flat();
+}
+
+/**
+ * Konverterer fellesaktivitetene til hendelser (events) som FullCalendar forstår.
+ * @param {string} [skoleaar] Valgfritt skoleår (f.eks. "2026-2027")
+ */
+function getFellesaktiviteterSomEvents(skoleaar) {
+  const liste = getFellesaktiviteter(skoleaar);
+
+  return liste
+    .filter(a => a.dato || a.fraDato) // Fjerner uavklarte datoer
+    .map(a => {
+      let start = a.dato || a.fraDato;
+      let end = a.dato || a.tilDato;
+
+      return {
+        id: a.id,
+        title: `[Felles] ${a.tittel}`,
+        start: start,
+        end: end,
+        allDay: true,
+        backgroundColor: '#8b5cf6', // Lilla bakgrunnsfarge
+        borderColor: '#7c3aed',
+        extendedProps: {
+          group: 'Felles',
+          rawTitle: a.tittel,
+          deltakere: a.deltakere,
+          ansvar: a.ansvar,
+          description: `Deltakere: ${a.deltakere} | Ansvar: ${a.ansvar}`
+        }
+      };
+    });
+}
