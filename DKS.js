9e4641676f19cc78cfe2a818881c9afd9a3eef4d
77tor <77tor@ikrs.no>
2026-08-10 12:03:31 +0200

    Add files via upload

---
+++ DKS.js
@@ -0,0 +1,374 @@
+/**
+ * DKS (Den Kulturelle Skolesekken) – Hånes skole 2026-2027
+ */
+
+const DKS_REGISTER = [
+  {
+    id: "dks-1",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-08",
+    startTid: "09:15",
+    sluttTid: "10:45",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "1A (24 per)"
+  },
+  {
+    id: "dks-2",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-08",
+    startTid: "11:45",
+    sluttTid: "13:15",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "1b (24 per)"
+  },
+  {
+    id: "dks-3",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-09",
+    startTid: "10:15",
+    sluttTid: "11:45",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "2a (17 per)"
+  },
+  {
+    id: "dks-4",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-10",
+    startTid: "09:15",
+    sluttTid: "10:45",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "2b (17 per)"
+  },
+  {
+    id: "dks-5",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-10",
+    startTid: "11:45",
+    sluttTid: "13:15",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "2C (23 per)"
+  },
+  {
+    id: "dks-6",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-11",
+    startTid: "09:15",
+    sluttTid: "10:45",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "3a (22 per)"
+  },
+  {
+    id: "dks-7",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-11",
+    startTid: "11:45",
+    sluttTid: "13:15",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "3b (20 per)"
+  },
+  {
+    id: "dks-8",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-14",
+    startTid: "09:15",
+    sluttTid: "10:45",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "3C (18 per)"
+  },
+  {
+    id: "dks-9",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-14",
+    startTid: "12:30",
+    sluttTid: "14:00",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "4A (22 per)"
+  },
+  {
+    id: "dks-10",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-15",
+    startTid: "09:15",
+    sluttTid: "10:45",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "4b (23 per)"
+  },
+  {
+    id: "dks-11",
+    tittel: "Mosaikk 26-27",
+    dato: "2026-09-15",
+    startTid: "11:45",
+    sluttTid: "13:15",
+    sted: "Hånes skole",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "4C (19 per)"
+  },
+  {
+    id: "dks-12",
+    tittel: "Kunst Nå! 26-27",
+    dato: "2026-09-21",
+    startTid: "09:15",
+    sluttTid: "10:45",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "5A (22 per)"
+  },
+  {
+    id: "dks-13",
+    tittel: "Kunst Nå! 26-27",
+    dato: "2026-09-21",
+    startTid: "11:45",
+    sluttTid: "13:15",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "5b (21 per)"
+  },
+  {
+    id: "dks-14",
+    tittel: "Kunst Nå! 26-27",
+    dato: "2026-09-22",
+    startTid: "09:15",
+    sluttTid: "10:45",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "5C (19 per)"
+  },
+  {
+    id: "dks-15",
+    tittel: "Kunst Nå! 26-27",
+    dato: "2026-09-22",
+    startTid: "11:45",
+    sluttTid: "13:15",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "6A (22 per)"
+  },
+  {
+    id: "dks-16",
+    tittel: "Kunst Nå! 26-27",
+    dato: "2026-09-23",
+    startTid: "09:15",
+    sluttTid: "10:45",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "6b (20 per)"
+  },
+  {
+    id: "dks-17",
+    tittel: "Kunst Nå! 26-27",
+    dato: "2026-09-23",
+    startTid: "11:45",
+    sluttTid: "13:15",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "6C (23 per)"
+  },
+  {
+    id: "dks-18",
+    tittel: "Kunst Nå! 26-27",
+    dato: "2026-09-24",
+    startTid: "09:15",
+    sluttTid: "10:45",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "6D (16 per)"
+  },
+  {
+    id: "dks-19",
+    tittel: "Kunst Nå! 26-27",
+    dato: "2026-09-24",
+    startTid: "11:45",
+    sluttTid: "13:15",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "7A (22 per)"
+  },
+  {
+    id: "dks-20",
+    tittel: "Kunst Nå! 26-27",
+    dato: "2026-09-25",
+    startTid: "09:15",
+    sluttTid: "10:45",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "7B (16 per)"
+  },
+  {
+    id: "dks-21",
+    tittel: "Kunst Nå! 26-27",
+    dato: "2026-09-25",
+    startTid: "11:45",
+    sluttTid: "13:15",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "7C (21 per)"
+  },
+  {
+    id: "dks-22",
+    tittel: "ARIADNES TRÅD",
+    dato: "2026-12-03",
+    startTid: "09:00",
+    sluttTid: "11:00",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Kristiansand kommune",
+    deltakere: "6b (20 per), 6A (22 per)"
+  },
+  {
+    id: "dks-23",
+    tittel: "ARIADNES TRÅD",
+    dato: "2026-12-03",
+    startTid: "12:00",
+    sluttTid: "14:00",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Kristiansand kommune",
+    deltakere: "6C (23 per), 6D (16 per)"
+  },
+  {
+    id: "dks-24",
+    tittel: "I Havets Favn 26-27",
+    dato: "2027-01-18",
+    startTid: "10:15",
+    sluttTid: "11:00",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "5.-6. trinn (103 deltakere)"
+  },
+  {
+    id: "dks-25",
+    tittel: "I Havets Favn 26-27",
+    dato: "2027-01-18",
+    startTid: "11:45",
+    sluttTid: "12:30",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "6.-7. trinn (96 deltakere)"
+  },
+  {
+    id: "dks-26",
+    tittel: "SANS 26-27",
+    dato: "2027-05-13",
+    startTid: "09:15",
+    sluttTid: "10:00",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "1.-3. trinn (152 deltakere)"
+  },
+  {
+    id: "dks-27",
+    tittel: "SANS 26-27",
+    dato: "2027-05-13",
+    startTid: "10:15",
+    sluttTid: "11:00",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "3.-5. trinn (143 deltakere)"
+  },
+  {
+    id: "dks-28",
+    tittel: "SANS 26-27",
+    dato: "2027-05-14",
+    startTid: "09:15",
+    sluttTid: "10:00",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "5.-7. trinn (156 deltakere)"
+  },
+  {
+    id: "dks-29",
+    tittel: "To i båden",
+    dato: "2027-05-18",
+    startTid: "09:15",
+    sluttTid: "09:55",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Kristiansand kommune",
+    deltakere: "1A (24 per), 1b (24 per)"
+  },
+  {
+    id: "dks-30",
+    tittel: "To i båden",
+    dato: "2027-05-18",
+    startTid: "11:45",
+    sluttTid: "12:25",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Kristiansand kommune",
+    deltakere: "2a (17 per), 2b (17 per), 2C (23 per)"
+  },
+  {
+    id: "dks-31",
+    tittel: "To i båden",
+    dato: "2027-05-19",
+    startTid: "09:15",
+    sluttTid: "09:55",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Kristiansand kommune",
+    deltakere: "3a (22 per), 3b (20 per), 3C (18 per)"
+  },
+  {
+    id: "dks-32",
+    tittel: "To i båden",
+    dato: "2027-05-19",
+    startTid: "11:45",
+    sluttTid: "12:25",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Kristiansand kommune",
+    deltakere: "4A (22 per), 4b (23 per), 4C (19 per)"
+  },
+  {
+    id: "dks-33",
+    tittel: "ELLE MAIJA 26-27",
+    dato: "2027-05-28",
+    startTid: "10:15",
+    sluttTid: "11:00",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "1.-2. trinn (130 deltakere)"
+  },
+  {
+    id: "dks-34",
+    tittel: "ELLE MAIJA 26-27",
+    dato: "2027-05-28",
+    startTid: "11:45",
+    sluttTid: "12:30",
+    sted: "Hånes skole – Gymsal",
+    arrangor: "Agder fylkeskommune",
+    deltakere: "3.-4. trinn (122 deltakere)"
+  }
+];
+
+export function getDKSAktiviteter() {
+  return DKS_REGISTER;
+}
+
+export function getDKSAktiviteterSomEvents() {
+  const liste = getDKSAktiviteter();
+
+  return liste.map(a => {
+    return {
+      id: a.id,
+      title: `[DKS] ${a.tittel} (${a.deltakere})`,
+      start: `${a.dato}T${a.startTid}:00`,
+      end: `${a.dato}T${a.sluttTid}:00`,
+      allDay: false,
+      backgroundColor: '#1abc9c',
+      borderColor: '#16a085',
+      extendedProps: {
+        group: 'DKS',
+        rawTitle: a.tittel,
+        sted: a.sted,
+        arrangor: a.arrangor,
+        deltakere: a.deltakere,
+        description: `Sted: ${a.sted} | Arrangør: ${a.arrangor} | Deltakere: ${a.deltakere}`
+      }
+    };
+  });
+}
